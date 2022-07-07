0d7ae8f94c01a4ab47dcae7706f4aec09dcf1caf
ehdrb92 <kcj920407@gmail.com>
2022-07-07 14:37:45 +0900

    Add : project models User classs

--- students/donggyu/users/models.py
+++ students/donggyu/users/models.py
@@ -5,3 +5,6 @@ class User(models.Model):
     email        = models.EmailField(max_length=254)
     password     = models.CharField(max_length=20)
     phone_number = models.CharField(max_length=15)
+
+    class Meta:
+        db_table = 'users'
