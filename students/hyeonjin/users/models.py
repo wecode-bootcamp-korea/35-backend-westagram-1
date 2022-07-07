6fad109040b38b73dbbe1e0c97d877d7c67f7abc
melony <hjz1010@gmail.com>
2022-07-07 16:51:04 +0900

    ADD : users 앱 생성 및 user 테이블 생성
    - users App: 사용자와 관련된 기능을 구현할 앱
    - User table: 사용자 정보 관리를 위한 클래스

---
+++ students/hyeonjin/users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model): 
+    name         = models.CharField()
+    email        = models.CharField()
+    password     = models.CharField()
+    phone_number = models.CharField()
