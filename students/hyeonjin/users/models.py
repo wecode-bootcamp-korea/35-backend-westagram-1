a81144523a4c7c859be3eb391758822145848cbb
melony <hjz1010@gmail.com>
2022-07-07 19:56:44 +0900

    Modify : models.py - 컨벤션에 맞지 않는 코드들 수정 - Meta class를 통해  데이터베이스 테이블 이름(복수) 추가

--- students/hyeonjin/users/models.py
+++ students/hyeonjin/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
-
-class User(models.Model):
-    name = models.CharField(max_length=45)
-    email = models.CharField(max_length=45, unique=True)
-    password = models.CharField(max_length=200)
+class User(models.Model): 
+    name         = models.CharField(max_length=45)
+    email        = models.CharField(max_length=45, unique=True)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=45)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
--- students/hyeonjin/users/views.py
+++ students/hyeonjin/users/views.py
@@ -1,26 +1,2 @@
-import json
-import re
-
-from django.http import JsonResponse
 
 from django.shortcuts import render
-from django.views import View
-
-# Create your views here.
-
-
-class JoinView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-
-        try:
-            email = data["email"]
-            password = data["password"]
-        except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        email_validation = re.compile(
-            '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        if not email_validation.match(data["email"]):
-            return JsonResponse({"message": "Email Validation Error"}, status=400)
-        if
