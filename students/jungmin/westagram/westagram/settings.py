cae9fc966e1a00139bcf008f1927b7fed93492f8
jeum <eumjungmin1@gmail.com>
2022-07-07 14:59:25 +0900

    수정: 모듈 import 구분

--- students/jungmin/westagram/westagram/settings.py
+++ students/jungmin/westagram/westagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+
 from my_settings import DATABASES, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
