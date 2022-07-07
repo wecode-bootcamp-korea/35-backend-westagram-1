a2532b9bccdc870ae333e70410bacfaef122b427
ehdrb92 <kcj920407@gmail.com>
2022-07-07 18:46:57 +0900

    Add : add User table column

--- students/donggyu/users/models.py
+++ students/donggyu/users/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 class User(models.Model): 
     name         = models.CharField(max_length=20)
-    email        = models.CharField(max_length=254)
-    password     = models.CharField(max_length=20)
+    email        = models.CharField(max_length=254, unique=True)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=15)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-    class Meta: 
+    class Meta:
         db_table = 'users'
