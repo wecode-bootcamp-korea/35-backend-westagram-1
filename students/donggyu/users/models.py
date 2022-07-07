da9cb5605484bb1be2b3904b82110c6fc52f28ea
ehdrb92 <kcj920407@gmail.com>
2022-07-07 14:07:32 +0900

    project models setting

---
+++ students/donggyu/users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+    name         = models.CharField(max_length=20)
+    email        = models.EmailField(max_length=254)
+    password     = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=15)
