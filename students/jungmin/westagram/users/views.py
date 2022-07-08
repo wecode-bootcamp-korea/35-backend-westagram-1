91ce9b5b9db2bd551573406df4a95145893bf2c3
jeum <eumjungmin1@gmail.com>
2022-07-08 14:29:06 +0900

    수정: UserView->SignUpView

--- students/jungmin/westagram/users/views.py
+++ students/jungmin/westagram/users/views.py
@@ -6,7 +6,7 @@ from django.views import View
 
 from users.models import User
 
-class UserView(View):
+class SignUpView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
