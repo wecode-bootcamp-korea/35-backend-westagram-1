cb7f97ca0e593cbfce10c505423b260770c79d3f
melony <hjz1010@gmail.com>
2022-07-08 09:42:09 +0900

    views.py 수정1
    - email 중복확인 오류 수정
    - others 오류 수정 진행 중

--- students/hyeonjin/users/models.py
+++ students/hyeonjin/users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class User(models.Model): 
+class User(models.Model):
     name         = models.CharField(max_length=45)
     email        = models.CharField(max_length=45, unique=True)
     password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=45)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
-    others       = models.TextField(Null=True, Blank=True)
+    # others       = models.TextField(null=True, blank=True)
 
     class Meta:
         db_table = 'users'
--- students/hyeonjin/users/views.py
+++ students/hyeonjin/users/views.py
@@ -8,60 +8,57 @@ from django.views import View
 from users.models import User
 
 class SignupView(View):
-    """
-    목적: 사용자 정보를 데이터베이스에 저장
-
-    1. client에게 사용자 정보를 받는다.
-    2. 받은 정보를 DB에 저장한다.
-    """
+    
+#    목적: 사용자 정보를 데이터베이스에 저장
+#
+#    1. client에게 사용자 정보를 받는다.
+#    2. 받은 정보를 DB에 저장한다.
+ 
     def post(self, request):
-        """
-        request.body = {
-            "name"        : "hyeonjin"
-            "email"       : "hhj@sample.com"
-            "password"    : "password12*"
-            "phone_number": "01012341234"
-            "others"      : "text"
-        }
-        """
+    
+ #       request.body = {
+ #           "name"        : "hyeonjin"
+ #           "email"       : "hhj@sample.com"
+ #           "password"    : "password12*"
+ #           "phone_number": "01012341234"
+ #           "others"      : "text"
+ #       }
+    
         data = json.loads(request.body)
 
         # 이메일이나 패스워드가 전달되지 않는 경우 KeyError 예외처리
         # 다른 데이터도 모두 KeyError 확인
         try:                       
-            email        = data["email"]
-            password     = data["password"]
-            name         = data["name"]
-            phone_number = data["phone_number"]
-            created_at   = data["created_at"]
-            updated_at   = data["updated_at"]
-            others       = data["others"]
+            email        = data['email']
+            password     = data['password']
+            name         = data['name']
+            phone_number = data['phone_number']
+            others       = data['others']
         except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
         # Email Validation # 이메일에 @와 .이 포함되어 있는지 확인하기 위해 정규표현식 활용 
         email_validation = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        if email_validation.match(data["email"]) == None:
-            return JsonResponse({"message": "Email Validation Error"}, status=400)
+        if email_validation.match(data['email']) == None:
+            return JsonResponse({'message': 'Email Validation Error'}, status=400)
 
         # Password Validation # 비밀번호 8자리 이상, 문자, 숫자, 특수문자 복합인지 확인
-        password_validation = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
+        password_validation = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$')
         if password_validation.match(data['password']) == None:
-            return JsonResponse({"message": "Password Validation Error"}, status=400)
+            return JsonResponse({'message': 'Password Validation Error'}, status=400)
 
-        try:
+        try: 
             User.objects.get(email=email)
+            return JsonResponse({'message': 'Duplicated E-mail'}, status=400)
         except User.DoesNotExist:
-            return JsonResponse({"message": "Duplicated E-mail"}, status=400)
-
+            pass
+        
         User.objects.create(
             name         = name,
             email        = email,
             password     = password,
             phone_number = phone_number,
-            created_at   = created_at,
-            updated_at   = updated_at,
-            others       = others
+            # others       = others
         )
 
-        return JsonResponse({"message":"SUCCESS"}, status=201)
+        return JsonResponse({'message':'SUCCESS'}, status=201)
