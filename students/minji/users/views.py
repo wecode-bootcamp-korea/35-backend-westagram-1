d9dd6314d817649bf03143e143cf7857d121fc98
mean-g <106012542+mean-g@users.noreply.github.com>
2022-07-08 17:13:51 +0900

    make sign-up view

--- students/minji/users/views.py
+++ students/minji/users/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+import json
+import re
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+from users.models import User
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data                = json.loads(request.body)
+            email               = data["email"]
+            password            = data["password"]
+            email_validation    = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
+            password_validation = re.compile("^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%*^&+=].*$)")
+
+            if email == "" or password == "":
+                return JsonResponse({"message":"KEY_ERROR"}, status=400)
+
+            if not email_validation.match(email):
+                return JsonResponse({"message":"EMAIL_VALIDATION_ERROR"}, status=400)
+
+            if not password_validation.match(password):
+                return JsonResponse({"message":"PASSWORD_VALIDATION_ERROR"}, status=400)
+            
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({"message":"DUPLICATION_ERROR"}, status=400)
+        
+            User.objects.create(
+             name          = data['name'],
+             email         = data['email'],
+             password      = data['password'],
+             mobile_number = data['mobile_number'],
+            )
+            return JsonResponse({'message':'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
