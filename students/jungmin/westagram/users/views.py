94ca78985d654fa7d47f627b4e4c664f789bc9d9
jeum <eumjungmin1@gmail.com>
2022-07-08 12:40:59 +0900

    수정: 이메일, 비밀번호 조건확인 변경, 이메일 중복확인법 변경

--- students/jungmin/westagram/users/views.py
+++ students/jungmin/westagram/users/views.py
@@ -1,7 +1,6 @@
 import json
 import re
 
-from django.db    import IntegrityError
 from django.http  import JsonResponse
 from django.views import View
 
@@ -17,14 +16,16 @@ class SignupView(View):
             password      = data['password']
             mobile_number = data['mobile_number']
 
-            email_validation = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-            if email_validation.match(email) == None:
+            REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
+
+            if not re.match(REGEX_EMAIL, email):
                 return JsonResponse({'message':'EMAIL_VALIDATION_FALSE'}, status=400)
-            
-            password_validation = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$')
-            if password_validation.match(password) == None:
+            if not re.match(REGEX_PASSWORD, password):
                 return JsonResponse({'message':'PASSWORD_VALIDATION_FALSE'}, status=400)
 
+            if User.objects.filter(email=email).exists():
+                return JsonResponse({'message': 'EMAIL_MUST_BE_UNIQUE'}, status=400)
 
             User.objects.create(
                 name          = name,
@@ -36,5 +37,3 @@ class SignupView(View):
             return JsonResponse({'message': 'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
-        except IntegrityError:
-            return JsonResponse({'message': 'EMAIL_MUST_BE_UNIQUE'}, status=400)
