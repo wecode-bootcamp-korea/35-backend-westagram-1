216ab6dc24bffe885a63624faa6d838a92630f00
melony <hjz1010@gmail.com>
2022-07-08 15:55:13 +0900

    views.py 수정3
    - 필요없는 주석 삭제
    - 변수로 선언해둔건 변수로!

--- students/hyeonjin/users/views.py
+++ students/hyeonjin/users/views.py
@@ -11,8 +11,6 @@ class SignupView(View):
     def post(self, request):
         data = json.loads(request.body)
 
-        # 이메일이나 패스워드가 전달되지 않는 경우 KeyError 예외처리
-        # 다른 데이터도 모두 KeyError 확인
         try:                       
             email        = data['email']
             password     = data['password']
@@ -20,15 +18,13 @@ class SignupView(View):
             phone_number = data['phone_number']
             others       = data['others']
             
-            # 이메일에 @와 .이 포함되어 있는지
-            # 비밀번호 8자리 이상, 문자, 숫자, 특수문자 복합인지- 확인하기 위해 정규표현식 활용 
             email_validation = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
             password_validation = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
-            if not re.match(email_validation, data['email']):
+            if not re.match(email_validation, email):
                 return JsonResponse({'message': 'Email Validation Error'}, status=400)
 
-            if not re.match(password_validation, data['password']):
+            if not re.match(password_validation, password):
                 return JsonResponse({'message': 'Password Validation Error'}, status=400)
             
             if User.objects.filter(email=email).exists():
