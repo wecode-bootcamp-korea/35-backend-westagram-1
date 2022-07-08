bf813079829b74810c342e1be02fea1d2e7ef1c4
jeum <eumjungmin1@gmail.com>
2022-07-08 14:25:33 +0900

    수정: models.py

--- students/jungmin/westagram/users/models.py
+++ students/jungmin/westagram/users/models.py
@@ -1,9 +1,8 @@
-from pickle import TRUE
 from django.db import models
 
 class User(models.Model):
     name          = models.CharField(max_length=45)
-    email         = models.CharField(max_length=200, unique=TRUE)
+    email         = models.CharField(max_length=200, unique=True)
     password      = models.CharField(max_length=200)
     mobile_number = models.CharField(max_length=100)
     created_at    = models.DateTimeField(auto_now_add=True)
