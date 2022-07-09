021c2659b0c7ece15dde886e290243590071d490
mean-g <106012542+mean-g@users.noreply.github.com>
2022-07-09 15:37:08 +0900

    make sign-up view fis

--- students/minji/users/views.py
+++ students/minji/users/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 import json
 import re
 
-from django.http import JsonResponse
+from django.http  import JsonResponse
 from django.views import View
 
 from users.models import User
@@ -13,16 +12,13 @@ class SignUpView(View):
             data                = json.loads(request.body)
             email               = data["email"]
             password            = data["password"]
-            email_validation    = re.compile("^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$")
-            password_validation = re.compile("^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%*^&+=].*$)")
+            REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
-            if email == "" or password == "":
-                return JsonResponse({"message":"KEY_ERROR"}, status=400)
-
-            if not email_validation.match(email):
+            if not re.match(REGEX_EMAIL, value):
                 return JsonResponse({"message":"EMAIL_VALIDATION_ERROR"}, status=400)
 
-            if not password_validation.match(password):
+            if not re.match(REGEX_PASSWORD, value):
                 return JsonResponse({"message":"PASSWORD_VALIDATION_ERROR"}, status=400)
             
             if User.objects.filter(email=email).exists():
