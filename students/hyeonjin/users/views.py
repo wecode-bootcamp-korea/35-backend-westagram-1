9b50626bc94716f6183f1f809129549345781c4e
melony <hjz1010@gmail.com>
2022-07-11 17:33:22 +0900

    정규표현식 글로벌 변수로 지정하는 위치 잘못되어 있던 것 수정

--- students/hyeonjin/users/views.py
+++ students/hyeonjin/users/views.py
@@ -8,9 +8,6 @@ from django.views import View
 from users.models import User
 
 class SignupView(View):
-
-    EMAIL_VALIDATION    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    PASSWORD_VALIDATION = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
     
     def post(self, request):
         data = json.loads(request.body)
@@ -22,8 +19,11 @@ class SignupView(View):
             phone_number = data['phone_number']
             others       = data['others']
 
-            global EMAIL_VALIDATION
-            global PASSWORD_VALIDATION
+            # global EMAIL_VALIDATION
+            # global PASSWORD_VALIDATION
+
+            EMAIL_VALIDATION    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+            PASSWORD_VALIDATION = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$'
 
             if not re.match(EMAIL_VALIDATION, email):
                 return JsonResponse({'message': 'Email Validation Error'}, status=400)
@@ -35,7 +35,7 @@ class SignupView(View):
                 return JsonResponse({'message': 'E-mail Duplicate'}, status=400)
 
             encoded_password = password.encode('utf-8')
-            hashed_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt)
+            hashed_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
 
             User.objects.create(
                 name         = name,
@@ -58,7 +58,7 @@ class LoginView(View):
             password = data['password']
 
             encoded_password = password.encode('utf-8')
-            hashed_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt)            
+            hashed_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt())            
 
             if not User.objects.filter(email=email, password=hashed_password).exists():
                 return JsonResponse({"message":"INVALID_USER"}, status=401)
