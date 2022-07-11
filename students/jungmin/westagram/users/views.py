c79889fa751b0744d139655d5f4eb4cfb865f6b4
jeum <eumjungmin1@gmail.com>
2022-07-11 16:51:41 +0900

    수정: views.py 예외사항 모두 처리후 로그인 성공 반환

--- students/jungmin/westagram/users/views.py
+++ students/jungmin/westagram/users/views.py
@@ -46,9 +46,9 @@ class SignInView(View):
             email    = data['email']
             password = data['password']
 
-            if User.objects.filter(email=email, password=password).exists():
-                return JsonResponse({'message':'SUCCESS'}, status=200)
-            else:
+            if not User.objects.filter(email=email, password=password).exists():
                 return JsonResponse({'message':'INVALID_USER'}, status=401)
+            
+            return JsonResponse({'message':'SUCCESS'}, status=200)
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
