a5aec3616aa90837e60a36bf0b0a723ddced3dfb
jeum <eumjungmin1@gmail.com>
2022-07-11 18:57:10 +0900

    수정: views.py 필요없는 변수 제거

--- students/jungmin/westagram/users/views.py
+++ students/jungmin/westagram/users/views.py
@@ -28,14 +28,12 @@ class SignUpView(View):
             if User.objects.filter(email=email).exists():
                 return JsonResponse({'message': 'EMAIL_MUST_BE_UNIQUE'}, status=400)
 
-            encoded_password = password.encode('utf-8')
-            hashed_password  = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
-            decoded_password = hashed_password.decode('utf-8')
+            hashed_password  = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt().decode('utf-8'))
 
             User.objects.create(
                 name          = name,
                 email         = email,
-                password      = decoded_password,
+                password      = hashed_password,
                 mobile_number = mobile_number
             )
 
