9f40114271ab4fac0b8eb3e05055ac0aceb9bdaa
melony <hjz1010@gmail.com>
2022-07-11 13:21:40 +0900

    Login View 수정요청 반영
    - email과 password를 동시에 만족하는 User를 조회하는 코드로 수정
    -> 가독성 및 로직 개선
    -> DoesNotExit 예외 따로 처리할 필요 X

--- students/hyeonjin/users/views.py
+++ students/hyeonjin/users/views.py
@@ -53,14 +53,12 @@ class LoginView(View):
             email    = data['email']
             password = data['password']
 
-            usr = User.objects.get(email=email)
-            if usr.password == password:
+            if User.objects.filter(email=email, password=password).exists():
                 return JsonResponse({"message":"SUCCESS"}, status=200)
-            else:
-                return JsonResponse({"message":"INVALID_USER"}, status=401)
-                
+            return JsonResponse({"message":"INVALID_USER"}, status=401)    
+
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
-        except User.DoesNotExist :
-            return JsonResponse({"message":"INVALID_USER"}, status=401)
+
+
 
