c1d684cb61e3061eb20e8b8292f724253792be14
jeum <eumjungmin1@gmail.com>
2022-07-12 14:38:02 +0900

    수정: views.py 정렬

--- students/jungmin/westagram/users/views.py
+++ students/jungmin/westagram/users/views.py
@@ -4,7 +4,7 @@ from django.http  import JsonResponse
 from django.views import View
 
 from users.models import User
-from my_settings import SECRET_KEY, ALGORITHM
+from my_settings  import SECRET_KEY, ALGORITHM
 
 class SignUpView(View):
     def post(self, request):
