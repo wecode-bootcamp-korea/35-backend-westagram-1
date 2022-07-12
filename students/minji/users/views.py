a5e8d2d7a6263cf053dcbd2865580062677113e8
mean-g <106012542+mean-g@users.noreply.github.com>
2022-07-12 17:13:08 +0900

    log in - jwt

--- students/minji/users/views.py
+++ students/minji/users/views.py
@@ -1,4 +1,4 @@
-import json, re, bcrypt
+import json, re, bcrypt, jwt
 
 from django.http  import JsonResponse
 from django.views import View
@@ -9,6 +9,7 @@ class SignUpView(View):
     def post(self, request):
         try:
             data           = json.loads(request.body)
+
             name           = data['name']
             email          = data["email"]
             password       = data["password"]
@@ -42,12 +43,17 @@ class LogInView(View):
     def post(self, request):
         try:
             data     = json.loads(request.body)
+
             email    = data['email']
             password = data['password']
 
-            if not User.objects.filter( email = email, password = password).exists():
+            user = User.objects.get(email = email)
+
+            if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')): 
                 return JsonResponse({'message':'INVALID_USER'}, status=401)
-            return JsonResponse({'message':'SUCCESS'}, status=200)
-        
+
+            token = jwt.encode({"user_id":user.id}, 'secret', algorithm="HS256")
+            return JsonResponse({"message":"SUCCESS", "access_token": token}, status=200)
+
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
